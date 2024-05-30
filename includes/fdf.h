c05563e37212c65c62e488c8e7a1f9abaf62efcb
dabochko <dabochko@student.42barcelona.com>
2024-05-30 14:46:46 +0200

    Define vector and matrix structures and their creation functions

--- includes/fdf.h
+++ includes/fdf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dabochko <dabochko@student.42barcel>       +#+  +:+       +#+        */
+/*   By: dabochko <dabochko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 16:27:31 by dabochko          #+#    #+#             */
-/*   Updated: 2024/05/29 16:28:43 by dabochko         ###   ########.fr       */
+/*   Updated: 2024/05/30 14:31:49 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,30 @@
 
 # define LENGHT 1920
 # define HEIGTH 1080
+
+// Keycodes
+
+#  define KEY_ESC 53
+
+# endif
+
+// STRUCTURES
+// Vector
+typedef struct s_vector
+{
+    float  x;
+    float  y;
+    float  z;
+}   t_vector;
+
+t_vector	create_vector(float x, float y, float z);
+
+// Matrix
+typedef struct s_matrix
+{
+    t_vector  vx;
+    t_vector  vy;
+    t_vector  vz;
+}   t_matrix;
+
+t_matrix	create_matrix(t_vector vx, t_vector vy, t_vector vz);
